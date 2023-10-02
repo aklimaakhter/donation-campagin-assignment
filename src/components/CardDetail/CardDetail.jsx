1e5cde59964ba11ac7837067762c06dfeac54cdf
Aklima Akhter <aklima00191@gmail.com>
2023-10-02 22:27:32 +0600

    localstorage data set up

--- src/components/CardDetail/CardDetail.jsx
+++ src/components/CardDetail/CardDetail.jsx
@@ -1,23 +1,45 @@
+/* eslint-disable react/prop-types */
 
 const CardDetail = ({ card }) => {
-    const { image, title, description, price, buttonBgColor } = card || {};
+    const {id, image, title, description, price, buttonBgColor } = card || {};
     const buttonStyle = {
         backgroundColor: buttonBgColor
 
     };
+
+    const handleDonation = () => {
+        const addedDonationCard = [];
+        const donationCard = JSON.parse(localStorage.getItem('donate'));
+        if (!donationCard) {
+            addedDonationCard.push(card);
+            localStorage.setItem('donate', JSON.stringify(addedDonationCard));
+        }
+        else {
+            const isExits = donationCard.find(card => card.id === id);
+            if (!isExits) {
+                addedDonationCard.push(...donationCard, card);
+                localStorage.setItem('donate', JSON.stringify(addedDonationCard));
+                alert('card added')
+            }
+            else{
+                alert('already added')
+            }
+
+        }
+    }
     return (
         <div>
             <div className="card card-compact  bg-base-100 shadow-xl ">
-                    <div className="">
-                        <figure><img src={image} alt="card" className=" w-full relative" /></figure>
-                        <div className="hero-overlay h-[30vh] bg-opacity-70 absolute bottom-32"></div>
-                    </div>
+                <div className="">
+                    <figure><img src={image} alt="card" className=" w-full relative" /></figure>
+                    <div className="hero-overlay h-[25vh] bg-opacity-70 absolute bottom-32"></div>
+                </div>
 
 
-                    <div className="card-actions absolute z-1 bottom-44 left-20 rounded" style={buttonStyle} >
-                        <button className="text-xl text-white px-6 py-4 rounded">Donate  {price}</button>
-                    </div>
-               
+                <div className="card-actions absolute z-1 bottom-44 left-20 rounded" style={buttonStyle} >
+                    <button onClick={handleDonation} className="text-xl text-white px-6 py-4 rounded">Donate  {price}</button>
+                </div>
+
                 <div className="card-body">
                     <h2 className="card-title">{title}</h2>
                     <p>{description}</p>
