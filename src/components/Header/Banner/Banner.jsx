d53c577e18ff2d2c6a3f59fab054de73be4e3de3
Aklima Akhter <aklima00191@gmail.com>
2023-10-03 20:41:26 +0600

    updated

--- src/components/Header/Banner/Banner.jsx
+++ src/components/Header/Banner/Banner.jsx
@@ -1,6 +1,19 @@
+import { useState } from "react";
+import { Input, Button } from "@material-tailwind/react";
 import bannerImage from "../../../assets/images/background-image.jpg"
+import CardDetail from "../../CardDetail/CardDetail";
 
 const Banner = () => {
+    const [text, setText] = useState("");
+    const onChange = ({ target }) => setText(target.value);
+
+    const handleCategory = (e) => {
+        e.preventDefault();
+        const category ='';
+        if(category ==='Food'){
+            <CardDetail></CardDetail>
+        }
+    }
     return (
         <div className=''>
             <div className="hero " >
@@ -11,16 +24,27 @@ const Banner = () => {
                 <div className="hero-content text-center text-neutral-content">
                     <div className="">
                         <h1 className="mb-5 md:text-5xl font-bold">I Grow By Helping People In Need</h1>
-                        {/* <input type="text" placeholder="Search here" className="input input-bordered input-primary w-full max-w-xs" />
-                        <input type="button" value="Search" className="btn bg-red-500" /> */}
-                                                
-                        {/* <input className="w-[400px] p-3" type="text" placeholder="Search here..." />
-                        <button className="btn bg-red-500"><input type="submit" value="Search" /></button> */}
-                       
+                        <div className="relative flex w-full max-w-[600px] mx-auto">
+                            <Input
+                                type="text"
+                                placeholder="Search here"
+                                value={text}
+                                onChange={onChange}
+                                className="py-2 px-4 rounded"
+
+                            />
+                            <Button
+
+                                onClick={handleCategory}
+                                onChange={setText}
+                                color={text ? "gray" : "blue-gray"}
+                                className=" text-gr absolute right-1 top-1 rounded"
+                            >
+                                Search
+                            </Button>
+                        </div>
                     </div>
                 </div>
-
-
             </div>
         </div>
     );
